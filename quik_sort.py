9d66fa6e95586d2bd33e13dc6dddd075e77f0747
mooooona <lr_2014@163.com>
2022-02-08 14:22:25 +0800

    first commit

---
+++ quik_sort.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+'''
+@file   : quik_sort.py
+@Auther : liur
+@Data   :2021/10/20
+'''
+
+def quik_sort(nums):
+    n = len(nums)
+
+if __name__ == '__main__':
+    nums = [1,4,6,2,3,7]
+    quik_sort(nums)
